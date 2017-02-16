77cc078ed8193b7c548df834485a8cb304d61a3a
Woh Shon <wohshon@gmail.com>
2017-02-17 02:33:41 +0800

    first cut

--- www/js/hello.js
+++ www/js/hello.js
@@ -1,10 +1,35 @@
+
 document.getElementById('say_hello').onclick = function () {
   document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
   $fh.cloud(
       {
-        path: 'hello',
+        //path: 'hello',
+        path: 'hello/pub',
+        //path: 'connect',
         data: {
-          hello: document.getElementById('hello_to').value
+          hello: document.getElementById('hello_to').value,
+          clientId: document.getElementById('clientId').value
+        }
+      },
+      function (res) {
+        document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
+      },
+      function (code, errorprops, params) {
+        alert('An error occured: ' + code + ' : ' + errorprops);
+      }
+  );
+};
+
+document.getElementById('say_hello1').onclick = function () {
+  document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
+  $fh.cloud(
+      {
+        //path: 'hello',
+        path: 'hello',
+        //path: 'connect',
+        data: {
+          hello: document.getElementById('hello_to').value,
+          clientId: document.getElementById('clientId').value
         }
       },
       function (res) {
--- www/js/init.js
+++ www/js/init.js
@@ -1,2 +1,29 @@
 //browerify script to load the feedhenry javascript sdk into a global variable
 window.$fh = require('fh-js-sdk');
+
+var randomValue=Math.floor((Math.random() * 1000000) + 1)+'_'+(Date.now());
+if (document.getElementById('clientId').value=='') {
+	document.getElementById('clientId').value=randomValue;
+}
+
+//alert(randomValue);
+//alert(document.getElementById('hello_to').value);
+//connect to mq
+ $fh.cloud(
+      {
+        //path: 'hello',
+        path: '/hello/connect',
+        //method: post,
+        data: {
+  //        hello: document.getElementById('hello_to').value,
+          clientId: document.getElementById('clientId').value
+        }
+      },
+      function (res) {
+        document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
+      },
+      function (code, errorprops, params) {
+        alert('An error occured: ' + code + ' : ' + errorprops);
+      }
+  );
+
